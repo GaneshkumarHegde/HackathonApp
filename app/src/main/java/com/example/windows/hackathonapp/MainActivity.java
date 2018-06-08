8750d75a47dbb96e161d54a02c34a0519777d331
Administrator <VikasGanesh>
2018-06-08 20:16:20 +0530

    signin and signup ui

--- app/src/main/java/com/example/windows/hackathonapp/MainActivity.java
+++ app/src/main/java/com/example/windows/hackathonapp/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.windows.hackathonapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        t = (TextView) findViewById(R.id.signuptxt);
+
+          t.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              Intent signuppage=new Intent(MainActivity.this,Signup.class);
+              startActivity(signuppage);
+          }
+      });
     }
 }
